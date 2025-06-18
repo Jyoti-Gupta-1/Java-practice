d144ba51a32dfc19adf232b388164eccfa439fb6
Jyoti Gupta <gupta.jyoti0811@gmail.com>
2025-06-18 10:44:15 +0530

    add day_3

---
+++ day_3/char.java
@@ -0,0 +1,10 @@
+public class Char{
+    public static void main(String[] args) {
+        float floatnum = 5.555f; 
+        int intnum = (int) floatnum; 
+        byte bytenum= (byte)intnum;
+        System.out.println("The float value is: " + floatnum);
+        System.out.println("its converted to: " + intnum);
+       System.out.println("its converted to: " + bytenum);
+    }
+}
---
+++ day_3/main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[]args){
+Scanner scanner =new Scanner(System.in);
+System.out.println("enter the integer:");
+        int n = scanner.nextInt();
+        System.out.println("the integer is:" + n);
+        scanner.close();
+    }
+}
