0de2a8d7b0e9fe0846a45d9589ee6922c467150e
Jyoti Gupta <gupta.jyoti0811@gmail.com>
2025-06-18 12:12:06 +0530

    add day_3

---
+++ day_3/phone.java
@@ -0,0 +1,12 @@
+import java.util.Sacanner;
+
+public class phone{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the phone number:");
+        String phoneNumber = sc.nextLine();
+         long [] phoneNumber = new long[n];
+        System.out.println("The phone number is: " + phoneNumber);
+        sc.close();
+    }
+}
