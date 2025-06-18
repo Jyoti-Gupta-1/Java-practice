75202bf79c3b7fc377afdaab68804676aed16dd8
Jyoti Gupta <gupta.jyoti0811@gmail.com>
2025-06-18 11:06:21 +0530

    add square

---
+++ day_3/square.java
@@ -0,0 +1,10 @@
+public class Char{
+      static void square(int a){
+            System.out.println("square of number:"+ a*a); 
+            }
+            
+    public static void main(String[] args) {
+       square (7);
+       square (8);
+       }
+}
